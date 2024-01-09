75184d28196ca3ea42a1ad441d5b3304932d837e
KiHwan Yun <drg2524@naver.com>
2024-01-09 17:39:35 +0900

    Exception Handler and Error Page result code commit

--- src/main/java/hello/exception/filter/LogFilter.java
+++ src/main/java/hello/exception/filter/LogFilter.java
@@ -10,6 +10,29 @@ import java.util.UUID;
 @Slf4j
 public class LogFilter implements Filter {
 
+    /*
+    *   [ DispatcherType ]
+    *
+    *   필터는 이런 경우를 위해서 DispatcherType라는 옵션을 제공한다.
+    *   이전 강의의 마지막에 다음 로그를 추가했다.
+    *   log.info("dispatchType={}", request.getDispatcherType())
+    *   그리고 출력해보면 오류 페이지에서 dispatchType=ERROR 로 나오는 것을 확인할 수 있다.
+    *
+    *   고객이 처음 요청하면 dispatcherType=REQUEST 이다.
+    *   이렇듯 서블릿 스펙은 실제 고객이 요청한 것인지, 서버가 내부에서 오류 페이지를 요청하는 것인지
+    *   DispatcherType으로 구분할 수 있는 방법을 제공한다.
+    *
+    *   REQUEST : 클라이언트 요청
+    *   ERROR : 오류 요청
+    *   FORWARD : MVC에서 배웠던 서블릿에서 다른 서블릿이나 JSP를 호출할 때 -> RequestDispatcher.forward(request, response);
+    *   INCLUDE : 서블릿에서 다른 서블릿이나 JSP의 결과를 포함할 때 -> RequestDispatcher.include(request, response);
+    *   ASYNC : 서블릿 비동기 호출
+    *
+    * */
+
+
+
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         log.info("log filter init");
